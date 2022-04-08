bfdf95d06536c75e3620da46b763d87f7d3ef2d3
Шаркунов Никита Ильич <Nikita.Sharkunov@urfu.me>
2022-04-08 13:43:09 +0500

    task-interfaces

--- src/Interfaces.ts
+++ src/Interfaces.ts
@@ -11,7 +11,7 @@ export function getPerson(): IPersonData {
     return {
         name: 'Petr',
         secondName: 'Smith',
-        age: '7',
+        age: 7,
     };
 }
 
@@ -27,6 +27,25 @@ export interface IStudentData {
 Данные должны храниться в объектах, соответствующих интерфейсу `IStudentData`.
  */
 export function getStudentsData(): IStudentData[] {
+    let ivan: IStudentData = {
+        name: 'Ivan',
+        secondName: 'Petrov',
+        age: 20,
+        phone: '+7(555)555-55-50',
+    };
+    let stepan: IStudentData = {
+        name: 'Stepan',
+        secondName: 'Petrov',
+        age: 19,
+        phone: '+7(555)555-55-51',
+    };
+    let petr: IStudentData = {
+        name: 'Petr',
+        secondName: 'Ivanov',
+        age: 19,
+    };
+    return [ivan, stepan, petr];
+
     /**
      * Данные студентов которые нужно передать:
      *
